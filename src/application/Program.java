69b9cacb6383c4f20fb21e09917574941780cc31
AlexandroLuiz <alexandro.luiz.hilleshein@gmail.com>
2023-03-29 14:43:58 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,12 +13,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Department obj= new Department(1,"Books");
-		Seller seller = new Seller(1, "Bob","bob@gmail.com",new Date(),3000.0, obj);
+		//Department obj= new Department(1,"Books");
+		//Seller seller = new Seller(1, "Bob","bob@gmail.com",new Date(),3000.0, obj);
+		System.out.println("==== teste find by id =======");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		SellerDao sellerDaoJDBC = new SellerDaoJDBC(DB.getConnection());
 		
-		System.out.println(sellerDaoJDBC.findById(1));
+		System.out.println(sellerDaoJDBC.findById(3));
 	}
 
 }
